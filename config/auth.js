5f7347fdb371ee2c111fd06cf21f607e87ce31e2
root <root@com.haowang>
2014-08-27 09:59:58 -0400

    test

---
+++ config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+	'facebookAuth' : {
+		'clientID' 		: '1455960178020028', // your App ID
+		'clientSecret' 	: '978d7368604aeb97b0ac623a239a8a6f', // your App Secret
+		'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
+	},
+
+	'twitterAuth' : {
+		'consumerKey' 		: 'Dn2cKriIhulVFRIMNOP0vv221',
+		'consumerSecret' 	: '6iRoqAGw4qZQQjHpZ8L0NYF9geLZqADlWDEnWcOo3sZayB8eoX',
+		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
+	},
+
+	'googleAuth' : {
+		'clientID' 		: '524589899556-3coorh6vk4tr6ekbk0nbginphreg813m.apps.googleusercontent.com',
+		'clientSecret' 	: 'KSfPXKd7W067GjpX2O0KYMFi',
+		'callbackURL' 	: 'https://localhost:8080/oauth2callback'
+	}
+
+};
--- routes/route.js
+++ routes/route.js
@@ -62,10 +62,50 @@ module.exports = function(app, passport) {
 	// LOGOUT ==============================
 	// =====================================
 	app.get('/logout', function(req, res) {
-		req.logout();
-		res.redirect('/');
+		
+		
+  		req.session.destroy(function (err) {
+  			setTimeout(function() {
+   				res.redirect ("/");
+				}, 2000);
+    		
+    		//Inside a callback… bulletproof!
+  		});
 	});
+
+
+	app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+	// handle the callback after facebook has authenticated the user
+	app.get('/auth/facebook/callback',
+		passport.authenticate('facebook', {
+			successRedirect : '/profile',
+			failureRedirect : '/'
+		}));
+
+	app.get('/auth/twitter', passport.authenticate('twitter'));
+
+	// handle the callback after twitter has authenticated the user
+	app.get('/auth/twitter/callback',
+		passport.authenticate('twitter', {
+			successRedirect : '/profile',
+			failureRedirect : '/'
+		}));
+
+	app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+    // the callback after google has authenticated the user
+    app.get('/oauth2callback',
+            passport.authenticate('google', {
+                    successRedirect : '/profile',
+                    failureRedirect : '/'
+            }));
+
 };
+
+
+
+
 
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
